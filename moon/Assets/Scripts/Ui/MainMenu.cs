c9b450be244cea8b6fa0f8e31e54c75e4dbdbc90
Vladislav <legovladislav@gmail.com>
2023-04-15 00:31:30 +0300

    Main menu.

---
+++ moon/Assets/Scripts/Ui/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Ui
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void OnNewGameButtonClicked(string sceneName)
+        {
+            StartCoroutine(LoadScene(sceneName));
+        }
+
+        public void OnExitButtonClicked()
+        {
+            Application.Quit();
+        }
+        
+        private static IEnumerator LoadScene(string nextScene)
+        {
+            if (SceneManager.GetActiveScene().name == nextScene)
+            {
+                yield break;
+            }
+            
+            var waitNextScene = SceneManager.LoadSceneAsync(nextScene);
+            while (!waitNextScene.isDone)
+            {
+                yield return null;    
+            }
+        }
+    }
+}
---
+++ moon/Assets/Scripts/Ui/ScaleButton.cs
@@ -0,0 +1,29 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Ui
+{
+    public class ScaleButton : MonoBehaviour
+    {
+        [SerializeField] private float duration;
+
+        private Tweener _scalePositiveTween;
+        private Tweener _scaleNegativeTween;
+        
+        public void ScalePositive()
+        {
+            _scalePositiveTween = transform.DOScale(new Vector3(1.05f, 1.05f, 1), duration);
+        }
+
+        public void ScaleNegative()
+        {
+            _scaleNegativeTween = transform.DOScale(new Vector3(1f, 1f, 1f), duration);
+        }
+
+        private void OnDestroy()
+        {
+            _scaleNegativeTween.Kill();
+            _scalePositiveTween.Kill();
+        }
+    }
+}

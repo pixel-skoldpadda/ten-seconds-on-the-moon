253880f74eb15768147acb8d7f9e77060c5eca41
Vladislav <legovladislav@gmail.com>
2023-04-16 20:29:24 +0300

    Main music theme.

---
+++ moon/Assets/Scripts/Infrastructure/GameBootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using Loader;
+using UnityEngine;
+
+namespace Infrastructure
+{
+    public class GameBootstrapper : MonoBehaviour
+    {
+        private const string MenuScene = "MenuScene";
+        
+        [SerializeField] private SceneLoader sceneLoader;
+        
+        private void Start()
+        {
+            sceneLoader.LoadScene(MenuScene);
+        }
+    }
+}
